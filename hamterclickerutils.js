d6b11aecfc82f3caea1dda3b8a8eda0b562f8754
bmcisaa4324 <bmcisaa4324+git@sudburycdsb.ca>
2025-02-22 12:12:05 -0500

    Create hamterclickerutils.js

---
+++ hamterclickerutils.js
@@ -0,0 +1,31 @@
+class HamterClickerUtils {
+  getInfo() {
+    return {
+      id: 'hamterclickerutils',
+      name: 'Hamter Clicker Utils',
+      blocks: [
+        {
+          opcode: 'addcommas',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'add commas to [NUMBER]',
+          arguments: {
+            NUMBER: {
+              type: Scratch.ArgumentType.NUMBER
+            }
+          }
+        }
+      ]
+    };
+  }
+
+  addcommas() {
+    let numStr = args.NUMBER.toString();
+    let parts = numStr.split('.');
+    let integerPart = parts[0];
+    let decimalPart = parts.length > 1 ? '.' + parts[1] : '';
+    integerPart = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    return integerPart + decimalPart;
+  }
+}
+
+Scratch.extensions.register(new HelloWorld());
